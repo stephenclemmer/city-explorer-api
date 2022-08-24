5a708f5e2c8466b3a65dc1396e9444d6255c1a13
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-08-23 17:23:18 -0700

    required the JSON file, created a weather route, isolated dataToGroom, isolated dataToSend by mapping over  dataToGroom to create an array that only looks for the date and weather description, sent dataToSend.

--- server.js
+++ server.js
@@ -2,30 +2,53 @@
 
 console.log('YOUR first SERVER! Awesome!');
 
-const {response} = require('express');
+// const {response} = require('express');
 const express = require('express');
-
-const app = express();
+const cors = require('cors');
+const data = require('./data/weather.json');
 
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3002;
 
-app.listen(PORT,() => console.log(`We are up on PORT: ${PORT}`));
 
 // Route Section:
 //  Base route:
+const app = express();
+// allows one to communicate to external services i.e., APIs
+app.use(cors());
 
 app.get('/', (request, response) => {
   response.status(200).send('Welcome to our server');
 });
 
+// weather route
+app.get('/weather', (request, response) => {
+  // let lat = request.query.lat;
+  // let lon = request.query.lon;
+  let cityName = request.query.city;
+  // console.log(city);
+  let dataToGroom = data.find(city => city.city_name === cityName);
+  let dataToSend = dataToGroom.data.map(object => {
+    return new Forecast(object);
+  });
+  
+  response.status(200).send(dataToSend);
+});
 
-// catch all NEEDS TO BE AT THE BOTTO<
+class Forecast {
+  constructor(weatherObj){
+    this.date = weatherObj.valid_date;
+    this.description = weatherObj.weather.description;
+  }
+}
+
+// catch all NEEDS TO BE AT THE BOTTOM
 app.get('*', (request, response) => {
   response.status(404).send('This route does not exist');
 });
 
 
-  
+app.listen(PORT,() => console.log(`We are up on PORT: ${PORT}`));
+
 
