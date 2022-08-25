f97218ac08bb54ed116eb95ae6fe8dc76b0c13b1
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-08-24 22:30:28 -0700

    ACP after trouble shooting

--- server.js
+++ server.js
@@ -55,11 +55,18 @@ class Forecast {
 app.get('/movies', getMovies);
 
 async function getMovies(request, response, next) {
+  
   let city = request.query.city;
+  console.log(city);
+  console.log(process.env.REACT_APP_MOVIE_API_KEY)
   const movieURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&query=${city}`;
+
+
+
 
   try {
     let cityData = await axios.get(movieURL);
+
     let dataToSend = cityData.data.results.map(object => new Playing(object));
     response.status(200).send(dataToSend);
   }
